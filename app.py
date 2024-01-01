f83a7e99efe06483bc45541df30d7a844501c3b2
satyakommana75 <satyakommana75@gmail.com>
2024-01-01 11:57:18 +0530

    new updates

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 ## Addition Code i have changed and add c only
 ## add extra
-def addition(a,b,c):
-     print(a+b+c)
+print("hi")
+##def addition(a,b,c):
+  ##   print(a+b+c)
     
-def multiplication(a*b):
-    return a*b 
+##def multiplication(a*b):
+    ##return a*b 
